6a74a4363f53b55a1a79bf6b7d76a2ce94f0ec1c
ariccio3 <ar3home@gmail.com>
2017-09-25 17:01:24 -0700

    Added models user.js

--- mentor-me/models/users.js
+++ mentor-me/models/users.js
@@ -1,15 +1,69 @@
 module.exports = function(sequelize, DataTypes) {
   var Users = sequelize.define("Users", {
-    text: {
+    firstName: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1, 140]
+        len: [1, 20]
       }
     },
-    complete: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 20]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 10]
+      }
+    },
+    photo: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    zip: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    aboutMe: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    skill1: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    skill2: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    skill3: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    skill4: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    skill5: {
+      type: DataTypes.STRING,
+      allowNull: true,
     }
   });
   return Users;
