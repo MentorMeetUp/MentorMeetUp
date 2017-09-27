3253d6789e809cabb8e84ddcb419aa77f082bb68
Norman Cunanan <norman.cunanan17@gmail.com>
2017-09-27 13:17:17 -0700

    fixed search

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -21,14 +21,10 @@ class Home extends Component {
   handleFormSubmit = event => {
     // Preventing the default behavior of the form submit (which is to refresh the page)
     event.preventDefault();
-    if (!this.state.email || !this.state.password) {
-      alert("Please fill out all fields");
-    } else if (this.state.password.length < 6) {
-      alert(
-        `Choose a more secure password`
-      );
+    if (!this.state.search) {
+      alert("Please enter a skill you'd like to search for");
     } else {
-      // alert(`Hello ${this.state.firstName} ${this.state.lastName}`);
+      alert(`You searched for: ${this.state.search}`);
 
       this.setState({
         search: ""
@@ -36,9 +32,6 @@ class Home extends Component {
       });
     }
 
-    // this.setState({
-    //   password: ""
-    // });
   };
 
   render() {
@@ -52,12 +45,13 @@ class Home extends Component {
           <div className="col-lg-3"></div>
           <div className="col-lg-6"> 
             <form className="form">
+            <label for="search">Search for a Skill You'd Like to Learn</label>
               <input
                 value={this.state.search}
                 name="search"
                 onChange={this.handleInputChange}
                 type="text"
-                placeholder="Search"
+                placeholder="Search for a Skill"
               />
               <button className="btn btn-primary btn-md" onClick={this.handleFormSubmit}>Submit</button>
             </form>
@@ -68,59 +62,5 @@ class Home extends Component {
   }
 }
 
-// class Home extends Component {
-// state = {
-//   search: "",
-// };
 
-//    handleInputChange = event => {
-//     // Getting the value and name of the input which triggered the change
-//     let value = event.target.value;
-//     const name = event.target.name;
-
-//     if (name === "password") {
-//       value = value.substring(0, 15);
-//     }
-//     // Updating the input's state
-//     this.setState({
-//       [name]: value
-//     });
-//   };
-
-//   handleFormSubmit = event => {
-//     // Preventing the default behavior of the form submit (which is to refresh the page)
-//     event.preventDefault();
-//     if (!this.state.search) {
-//       alert("Please enter a skill you'd like to search for");
-//     } else {
-//       alert(`You searched for: ${this.state.search}`);
-//     }
-//   };
-
-//  render() {
-//     return (
-//     <div>
-//     <h1>Home Page</h1>
-//     	<div className="row">
-//        <div className="col-lg-3"></div>
-//         <div className="col-lg-6">        
-//             <form className="form">
-//               <label for="search">Search for a Skill You'd Like to Learn</label>
-//               <input
-//                 className="form-control"
-//                 value={this.state.search}
-//                 name="Search for a Skill"
-//                 onChange={this.handleInputChange}
-//                 type="text"
-//                 placeholder="Search for a Skill"
-//               />
-//             <button className="btn btn-primary btn-md" onClick={this.handleFormSubmit}>Submit</button>
-//           <div className="col-lg-3"></div>
-//     		</form>
-//         </div>
-//       	</div>
-//     </div>
-//  	)
-//  }                   
-// }
 export default Home;
