eb5c4cf0d1e58a85b0bdb74252a64cb530d1a4e8
ariccio3 <ar3home@gmail.com>
2017-09-29 20:30:04 -0700

    NoMatch set up

--- client/src/pages/noMatch/NoMatch.js
+++ client/src/pages/noMatch/NoMatch.js
@@ -1,21 +1,19 @@
-// import React from "react";
-// import { Col, Row, Container } from "../../components/Grid";
-// import Jumbotron from "../../components/Jumbotron";
+import React from "react";
 
-// const NoMatch = () =>
-//   <Container fluid>
-//     <Row>
-//       <Col size="md-12">
-//         <Jumbotron>
-//           <h1>404 Page Not Found</h1>
-//           <h1>
-//             <span role="img" aria-label="Face With Rolling Eyes Emoji">
-//               🙄
-//             </span>
-//           </h1>
-//         </Jumbotron>
-//       </Col>
-//     </Row>
-//   </Container>;
+const NoMatch = () =>
+  // <Container fluid>
+  //   <Row>
+  //     <Col size="md-12">
+        <div>
+          <h1>404 Page Not Found</h1>
+          <h1>
+            <span role="img" aria-label="Face With Rolling Eyes Emoji">
+              🙄
+            </span>
+          </h1>
+        </div>
+  //     </Col>
+  //   </Row>
+  // </Container>;
 
-// export default NoMatch;
+export default NoMatch;
