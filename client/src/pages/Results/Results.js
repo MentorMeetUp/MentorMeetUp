0e224ac78c9ebfc7e18ab4ab542024ebc6a13181
Norman Cunanan <norman.cunanan17@gmail.com>
2017-09-30 12:15:59 -0700

    Results page info added

--- client/src/pages/Results/Results.js
+++ client/src/pages/Results/Results.js
@@ -2,24 +2,66 @@ import React, { Component } from "react";
 // import { Link } from "react-router-dom";
 import "./Results.css";
 import API from "../../utils/API";
+// import Card from "../../components/Card";
 
 class Results extends Component {
   state = {
-    skill: {}
+    skill: []
   };
- 
+
   componentDidMount() {
-    API.getUserSkill(this.props.match.params.skill)
-      .then(res => console.log({ skill: res.data }))
-      .catch(err => console.log(err));
+    this.loadResults();
   }
+
+  loadResults = () => {
+    API.getUserSkill(this.props.match.params.skill)
+    .then(res => this.setState({ skill: res.data }))
+    .catch(err => console.log(err));
+  };
+
   	render() {
 	    return (
 			<div className="container">
-			    <h1>Results Page</h1>
+              {this.state.skill.length ? (
+                <div>
+                {this.state.skill.map(result => (
+                  <div>               
+                      <strong>
+                        {result.city} by {result.email}
+                      </strong>
+                  </div>
+                ))}
+                </div>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
 			</div>                  
 		);
 	}
-}                 
+}
+
+
+
+// class Results extends Component {
+//   state = {
+//     skill: {}
+//   };
+ 
+//   componentDidMount() {
+//     API.getUserSkill(this.props.match.params.skill)
+//       .then(res => console.log({ skill: res.data }))
+//       .catch(err => console.log(err));
+//   }
+  
+
+//   	render() {
+// 	    return (
+// 			<div className="container">
+// 			    <h1>Results Page  </h1>
+//           <Card />
+// 			</div>                  
+// 		);
+// 	}
+// }                 
 
 export default Results;
